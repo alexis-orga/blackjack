5001a9c8fcf301d202b59d25f97025eb99ccec50
alexis-orga <alexis.orga@icloud.com>
2025-03-17 17:49:48 +0100

    adding possibility to play again and fixing code to able it

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,60 @@
 import java.util.Scanner;
 
 public class Main {
+    @SuppressWarnings("resource")
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        Deck deck = new Deck();
-        Hand playerHand = new Hand();
-        Hand dealerHand = new Hand();
+        boolean playAgain = true;
 
-        playerHand.addCard(deck.drawCard());
-        dealerHand.addCard(deck.drawCard());
-        playerHand.addCard(deck.drawCard());
-        dealerHand.addCard(deck.drawCard());
+        while (playAgain) {
+            Deck deck = new Deck();
+            Hand playerHand = new Hand();
+            Hand dealerHand = new Hand();
 
-        while (true) {
+            playerHand.addCard(deck.drawCard());
+            dealerHand.addCard(deck.drawCard());
+            playerHand.addCard(deck.drawCard());
+            dealerHand.addCard(deck.drawCard());
+
             System.out.println("Your hand: " + playerHand + "Total: " + playerHand.getTotal());
             System.out.println("Dealer hand: [?] " + dealerHand.getCards().get(1));
-        if (playerHand.getTotal() == 21) {
-            System.out.println("Blackjack ! You win !");
-            return;
-        }
-
-        System.out.println("Do you want to draw a card? (yes/no)");
-        String choice = scanner.nextLine().toLowerCase();
-        if (choice.equals("yes")) {
-            playerHand.addCard(deck.drawCard());
-            System.out.println(playerHand);
-            if (playerHand.getTotal() > 21) {
-                System.out.println(playerHand.getTotal());
-                System.out.println("Busted, you are above 21 ! You lose :(");
-                return;
+            if (playerHand.getTotal() == 21) {
+                System.out.println("Blackjack ! You win !");
             }
-        } else {
-            break;
-        }
-        }
+            if (dealerHand.getTotal() == 21) {
+                System.out.println("Dealer got blackjack, you lost !");
+            }
 
-        System.out.println("Dealer has the hand");
-        while (dealerHand.getTotal() < 17) {
-            dealerHand.addCard(deck.drawCard());
-        }
+            System.out.println("Do you want to draw a card? (yes/no)");
+            String choice = scanner.nextLine().toLowerCase();
+            if (choice.equals("yes")) {
+                playerHand.addCard(deck.drawCard());
+                System.out.println(playerHand);
+                System.out.println(playerHand.getTotal());
+                if (playerHand.getTotal() > 21) {
+                    System.out.println("Busted, you are above 21 ! You lose :(");
+                    return;
+                }
+            } 
 
-        System.out.println("Dealer hand: " + dealerHand + " Total: " + dealerHand.getTotal());
+            System.out.println("Dealer has the hand");
+            while (dealerHand.getTotal() < 17) {
+                dealerHand.addCard(deck.drawCard());
+            }
 
-        if (dealerHand.getTotal() > 21 || playerHand.getTotal() > dealerHand.getTotal()) {
-            System.out.println("You won !");
-        } else if (dealerHand.getTotal() == playerHand.getTotal()) {
-            System.out.println("Draw !");
-        } else {
-            System.out.println("Dealer won !");
+            System.out.println("Dealer hand: " + dealerHand + " Total: " + dealerHand.getTotal());
+
+            if (dealerHand.getTotal() > 21 || playerHand.getTotal() > dealerHand.getTotal()) {
+                System.out.println("You won !");
+            } else if (dealerHand.getTotal() == playerHand.getTotal()) {
+                System.out.println("Draw !");
+            } else {
+                System.out.println("Dealer won !");
+            }
+
+            System.out.println("Do you want to play again ? (yes/no)");
+            String playAgainResponse = scanner.nextLine().toLowerCase();
+            playAgain = playAgainResponse.equals("yes");
         }
     }
 }
