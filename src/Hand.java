bf8fde44f00bd497bd4f4de98c93f75cfc532396
alexis-orga <alexis.orga@icloud.com>
2025-03-19 15:10:27 +0100

    fix draw as a player

--- src/Hand.java
+++ src/Hand.java
@@ -4,21 +4,21 @@ import java.util.List;
 public class Hand {
     private final List<Integer> cards = new ArrayList<>();
 
-    public void addCard(int card) {
-        cards.add(card);
-    }
-
-    public List<Integer> getCards() {
-        return cards;
-    }
-
-    public int getTotal() {
-        int total = 0;
-        int aceCount = 0;
-        for (int card : cards) {
-            if (card > 10) {
-                total += 10;
-            } else if (card == 1) {
+        public void addCard(int card) {
+            cards.add(card);
+        }
+    
+        public List<Integer> getCards() {
+            return cards;
+        }
+    
+        public int getTotal() {
+            int total = 0;
+            int aceCount = 0;
+            for (int card : cards) {
+                if (card > 10) {
+                    total += 10;
+                } else if (card == 1) {
                 total += 11;
                 aceCount ++;
             } else {
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         boolean playAgain = true;
+        boolean drawCard = true;
 
         while (playAgain) {
             Deck deck = new Deck();
@@ -24,22 +25,28 @@ public class Main {
             if (dealerHand.getTotal() == 21) {
                 System.out.println("Dealer got blackjack, you lost !");
             }
-
-            System.out.println("Do you want to draw a card? (yes/no)");
-            String choice = scanner.nextLine().toLowerCase();
-            if (choice.equals("yes")) {
-                playerHand.addCard(deck.drawCard());
-                System.out.println(playerHand);
-                System.out.println(playerHand.getTotal());
-                if (playerHand.getTotal() > 21) {
-                    System.out.println("Busted, you are above 21 ! You lose :(");
-                    return;
+            while (drawCard) {
+                System.out.println("Do you want to draw a card? (yes/no)");
+                String choice = scanner.nextLine().toLowerCase();
+                if (choice.equals("yes")) {
+                    playerHand.addCard(deck.drawCard());
+                    System.out.println(playerHand);
+                    System.out.println(playerHand.getTotal());
+                    if (playerHand.getTotal() > 21) {
+                        System.out.println("Busted, you are above 21 ! You lose :(");
+                        return;
+                    } else if (playerHand.getTotal() == 21) {
+                        System.out.println("You hit 21, cant draw again");
+                        break;
+                    }
+                } else {
+                    break;
                 }
-            } 
-
+            }
             System.out.println("Dealer has the hand");
             while (dealerHand.getTotal() < 17) {
                 dealerHand.addCard(deck.drawCard());
+                System.out.println(dealerHand + " " + dealerHand.getTotal());
             }
 
             System.out.println("Dealer hand: " + dealerHand + " Total: " + dealerHand.getTotal());
